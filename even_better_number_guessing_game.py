95e7fca61c18bf17b40b435ebbefc074181f88f5
Ubuntu <ubuntu@ip-172-31-81-184.ec2.internal>
2021-09-25 00:08:35 +0000

    reformatted another time

--- even_better_number_guessing_game.py
+++ even_better_number_guessing_game.py
@@ -11,23 +11,27 @@ def main():
     # this function creates the game
 
     # input
-    number_guessed = input("Enter the number between 0 - 9: ")
+    user_input = input("Enter the number between 0 - 9: ")
 
     # process
     random_number = random.randint(0, 9)  # a number between 0 and 9
     try:
-        number_guessed = int(number_guessed)
+        number_guessed = int(user_input)
+        not_integer = "false"
     except Exception:
-        print("{0} is not an integer.".format(number_guessed))
-
-    if number_guessed == random_number:
-        print("You guessed the number!")
-    else:
-        print("Incorrect, the number was {0}.".format(random_number))
+        not_integer = "true"
+    finally:
+        if not_integer == "true":
+            print("{0} is not an integer.".format(user_input))
+        elif number_guessed not in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
+            print("Incorrect, the number was {0}.".format(random_number))
+        elif number_guessed == random_number:
+            print("You guessed the number!")
+        else:
+            print("Incorrect, the number was {0}.".format(random_number))
 
     # output
     print("Thanks for playing.")
-
     print("\nDone.")
 
 
